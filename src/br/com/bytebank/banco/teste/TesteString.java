424ab190d70c2a07c39db3418fd46e2caebaf16c
Samuel <samuel.f.silva@outlook.com>
2022-08-02 08:55:28 -0300

    Atualição 0.0.1

---
+++ src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,21 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+
+        String nome = "Alura";
+
+        String nova = nome.replace("A", "a");
+        
+        System.out.println(nova);
+
+        String nova2 = nome.toUpperCase();
+        System.out.println(nova2);
+
+               
+    }
+
+    
+    
+}
